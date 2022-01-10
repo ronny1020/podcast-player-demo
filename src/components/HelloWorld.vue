9d9b0220946e0308f1a08eb32865a9f2fb34d62d
Ronny Hsieh <46809311+ronny1020@users.noreply.github.com>
2022-01-10 18:14:45 +0800

    chore(init): set up develope env

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -9,27 +9,11 @@ const count = ref(0)
 <template>
   <h1>{{ msg }}</h1>
 
-  <p>
-    Recommended IDE setup:
-    <a href="https://code.visualstudio.com/" target="_blank">VSCode</a>
-    +
-    <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
-  </p>
-
-  <p>See <code>README.md</code> for more information.</p>
-
-  <p>
-    <a href="https://vitejs.dev/guide/features.html" target="_blank">
-      Vite Docs
-    </a>
-    |
-    <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Docs</a>
-  </p>
-
   <button type="button" @click="count++">count is: {{ count }}</button>
   <p>
     Edit
-    <code>components/HelloWorld.vue</code> to test hot module replacement.
+    <code>components/HelloWorld.vue</code>
+    to test hot module replacement.
   </p>
 </template>
 
@@ -44,9 +28,9 @@ label {
 }
 
 code {
-  background-color: #eee;
   padding: 2px 4px;
   border-radius: 4px;
+  background-color: #eee;
   color: #304455;
 }
 </style>
