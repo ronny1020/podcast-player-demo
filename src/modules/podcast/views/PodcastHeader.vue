6d871a080958b7a39a12fa283dfe2d47671ce73e
Ronny Hsieh <46809311+ronny1020@users.noreply.github.com>
2022-01-13 14:43:41 +0800

    feat(podcast): add back icon to home page

--- src/modules/podcast/views/PodcastHeader.vue
+++ src/modules/podcast/views/PodcastHeader.vue
@@ -1,18 +1,30 @@
 <template>
   <Skeleton v-if="!channel" active />
-  <header v-else>
-    <Image :width="300" :src="channel?.image.url" alt="channel image" />
-    <div class="content">
-      <h1>{{ channel?.title }}</h1>
-      <p>{{ channel?.description }}</p>
+
+  <div v-else class="header-wrapper">
+    <div class="back-icon">
+      <router-link v-if="!atHomePage" to="/">
+        <ArrowLeftOutlined />
+      </router-link>
     </div>
-  </header>
+    <header>
+      <Image :width="300" :src="channel?.image.url" alt="channel image" />
+      <div class="content">
+        <h1>{{ channel?.title }}</h1>
+        <p>{{ channel?.description }}</p>
+      </div>
+    </header>
+  </div>
 </template>
 
 <script setup lang="ts">
 import { computed } from 'vue'
+import { ArrowLeftOutlined } from '@ant-design/icons-vue'
 import { Image, Skeleton } from 'ant-design-vue'
 import usePodcastListStore from '../store/podcastListStore'
+import router from '@/router'
+
+const atHomePage = computed(() => router.currentRoute.value.path === '/')
 
 const podcastListStore = usePodcastListStore()
 
@@ -20,6 +32,12 @@ const channel = computed(() => podcastListStore.rss?.channel)
 </script>
 
 <style lang="scss" scoped>
+.back-icon {
+  height: 50px;
+  font-size: 2rem;
+  cursor: pointer;
+}
+
 header {
   display: flex;
   align-items: center;
